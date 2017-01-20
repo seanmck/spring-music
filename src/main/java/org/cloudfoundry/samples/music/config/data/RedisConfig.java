462dd5ea51ae5d4427cb1e4f153529d791c0e794
Tom Collings <tcollings@ecsteam.com>
2017-01-20 09:20:19 -0700

    adding functionality for azure-provided redis service instance

--- src/main/java/org/cloudfoundry/samples/music/config/data/RedisConfig.java
+++ src/main/java/org/cloudfoundry/samples/music/config/data/RedisConfig.java
@@ -12,7 +12,7 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@Profile("redis")
+@Profile({ "redis", "azureredis" })
 public class RedisConfig {
 
     @Bean
